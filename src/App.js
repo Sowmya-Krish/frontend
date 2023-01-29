20252aff726567319f68e0a479778638b4735e2a
Sowmya-Krish <sowmikrish.crazy@gmail.com>
2023-01-30 05:12:00 +0530

    Web Scraper

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Loading from "./Loading";
 
-function App() {
+const App = () => {
+  const [url, setURL] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [websiteContent, setWebsiteContent] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setURL("");
+    sendURL();
+  };
+  const trimDescription = (content) =>
+    content.match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/"/g, "");
+
+  async function sendURL() {
+    try {
+      const request = await fetch("http://localhost:4000/api/url", {
+        method: "POST",
+        body: JSON.stringify({
+          url,
+        }),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await request.json();
+      if (data.message) {
+        setLoading(false);
+        setWebsiteContent(data.database);
+        console.log(data);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  if (loading) {
+    return <Loading />;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <form className="home__form">
+        <h2>Web Scraper</h2>
+        <label htmlFor="url">Provide the website URL</label>
+        <input
+          type="url"
+          name="url"
+          id="url"
+          value={url}
+          onChange={(e) => setURL(e.target.value)}
+        />
+        <button onClick={handleSubmit}>SCRAP</button>
+      </form>
+      <main className="website__container ">
+        {websiteContent.map((item) => (
+          <div className="website__item" key={item.id}>
+            <img src={item?.brandImage} alt={item?.brandName} />
+            <h3>{item?.brandName}</h3>
+            <p>{trimDescription(item?.brandDescription)}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
-}
+};
 
 export default App;
